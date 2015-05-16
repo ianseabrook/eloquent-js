e0666218bf9263cb87d11c67cc6a3fd13bc30f46
Ian Seabrook <ian@ianseabrook.com>
2015-05-16 18:09:06 +0100

    Added solution to Chess Board.

--- 2. Program Structure/Chess Board/chess.js
+++ 2. Program Structure/Chess Board/chess.js
@@ -15,3 +15,18 @@ Passing this string to console.log should show something like this:
 # # # #
 When you have a program that generates this pattern, define a variable size = 8 and change the program so that it works for any size, outputting a grid of the given width and height.
 */
+var gridSize = 8;
+
+var chessBoard = "";
+
+for (var y = 0; y < gridSize; y++) {
+  for (var x = 0; x < gridSize; x++) {
+    if ((x + y) % 2 === 0) {
+      chessBoard += " ";
+    } else {
+      chessBoard += "#";
+    }
+  } chessBoard += "\n"
+}
+
+console.log(chessBoard);
