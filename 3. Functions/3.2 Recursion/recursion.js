459caba71888e78b5de581757500a54bcf20d722
Ian Seabrook <ian@ianseabrook.com>
2015-05-17 14:10:20 +0100

    Added solution to 3.2 Recursion.

--- 3. Functions/3.2 Recursion/recursion.js
+++ 3. Functions/3.2 Recursion/recursion.js
@@ -16,6 +16,18 @@ Test it on 50 and 75. See how it behaves on -1. Why? Can you think of a way to f
 
 // Your code here.
 
+function isEven(n){
+  if (n === 0) {
+    return true;
+  } else if (n === 1) {
+    return false;
+  } else if (n < 0) {
+    return isEven(-n);
+  } else {
+    return isEven(n - 2);
+  }
+}
+
 console.log(isEven(50));
 // → true
 console.log(isEven(75));
