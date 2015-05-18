dc8f72b4acf2beae1e8211bcef50291b3c334b50
Ian Seabrook <ian@ianseabrook.com>
2015-05-18 16:24:57 +0100

    Added Solution to 4.3 A List

--- 4. Data Structures: Objects and Arrays/4.3 A List/list.js
+++ 4. Data Structures: Objects and Arrays/4.3 A List/list.js
@@ -24,6 +24,33 @@ If you haven’t already, also write a recursive version of nth.
 */
 
 // Your code here.
+function arrayToList(array) {
+  var list = null;
+  for (var i = array.length - 1; i >= 0; i--)
+    list = {value: array[i], rest: list};
+  return list;
+}
+
+function listToArray(list) {
+  var array = [];
+  for (var node = list; node; node = node.rest) {
+    array.push(node.value);
+  }
+  return array;
+}
+
+function prepend(value,list) {
+  return {value: value, rest: list}
+}
+
+function nth(list, n) {
+  if (!list)
+    return undefined;
+  else if (n == 0)
+    return list.value;
+  else
+    return nth(list.rest, n - 1);
+}
 
 console.log(arrayToList([10, 20]));
 // → {value: 10, rest: {value: 20, rest: null}}
